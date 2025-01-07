cf4a15d171b38115cd793845105746ba688a6648
Patrick Carlsen <paca0001@stud.kea.dk>
2025-01-07 21:04:54 +0100

    Færdig - i think

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const switchButton = document.getElementById("switch");
 const body = document.body;
 
-let isDarkMode = false;
+let isDarkMode = true;
 
 body.classList.add("darkmode");
 switchButton.src = "IMGS/darkbutton.svg";
